33edd8a41179587d28a1352e00fe07698f351e71
jaemin <lemon__96@naver.com>
2024-07-30 18:31:28 +0900

    fix: update module - user

--- src/User/user.module.ts
+++ src/User/user.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   providers: [UserService],
+  exports: [UserService],
   controllers: [UserController],
 })
 export class UserModule {}
