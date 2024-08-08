26cf0ac8b17cd723cf6c85005959cf4b605893fe
jaemin <lemon__96@naver.com>
2024-08-08 15:06:05 +0900

    feat & fix: TypeORM 환경 설정 및 레거시 파일정리

--- src/User/user.module.ts
+++ src/User/user.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { UserResolver } from './user.resolver';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   providers: [UserService, UserResolver, PrismaService],
   exports: [UserService],
-  controllers: [UserController],
 })
 export class UserModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './User/user.module';
-import { AppController } from './app.controller';
 import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      timezone: 'Z',
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: false,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
@@ -20,7 +31,6 @@ import { join } from 'path';
     PrismaModule,
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, { provide: APP_PIPE, useClass: ValidationPipe }],
+  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
 export class AppModule {}
